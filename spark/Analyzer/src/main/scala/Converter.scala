18cc63987adbd57c13b587405865554f854ad3da
Tomasz Ożański <tomasz.ozanski@microscopeit.com>
2019-01-18 13:02:41 +0100

    adding converter application

---
+++ spark/Analyzer/src/main/scala/Converter.scala
@@ -0,0 +1,39 @@
+package analyzer
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import org.apache.spark.storage.StorageLevel
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.SparkSession
+
+object Converter {
+  
+  def main(args: Array[String]) {
+    
+    if( args.length != 1 )
+      throw new RuntimeException("no prefix path given");
+    
+    args.foreach( println );
+    val pathPrefix = args(0);
+ 
+ 
+    val conf = new SparkConf().setAppName("SimBaD chronicles converter");
+    val sc = new SparkContext(conf);
+    val sqlContext = new SQLContext(sc);
+    val spark = sqlContext.sparkSession;
+    import spark.implicits._
+    
+    ChronicleLoader.loadEntries( spark, pathPrefix + "/chronicles.csv.gz" ).
+      repartition($"particleId").
+      write.
+      format("parquet").
+      save(pathPrefix+"/chronicles.parquet")
+  }
+}
