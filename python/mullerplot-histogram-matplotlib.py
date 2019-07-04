6cc6dbcde59cc23e0f0db6803fed2422eb817972
Marek Bawiec <Marek.Bawiec@pwr.edu.pl>
2019-07-05 00:46:20 +0200

    mullerplot-histogram v2

--- python/mullerplot-histogram-matplotlib.py
+++ python/mullerplot-histogram-matplotlib.py
@@ -22,15 +22,15 @@ def get_args(argv):
 
     try:
         opts, args = getopt.getopt(argv, "hi:t:s:p:o:", [
-                        "iFile=", "tFile", "sFile", "pFile", "oFile="])
+                        "iFile=", "tFile", "sFile", "oFile="])
     except getopt.GetoptError:
         print('test.py -i <inputFile> -t <timeFile> -s <statsFile> \
-              -p <paramsFile> -o <outputFile>')
+              -o <outputFile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print('test.py -i <inputFile> -t <timeFile> -s \
-                  <statsFile> -p <paramsFile> -o <outputFile>')
+                  <statsFile> -o <outputFile>')
             sys.exit()
         elif opt in ("-i", "--iFile"):
             inputFile = arg
@@ -38,12 +38,10 @@ def get_args(argv):
             timeFile = arg
         elif opt in ("-s", "--sFile"):
             statsFile = arg
-        elif opt in ("-p", "--pFile"):
-            paramsFile = arg
         elif opt in ("-o", "--oFile"):
             outputFile = arg
 
-    return (inputFile, timeFile, statsFile, paramsFile, outputFile)
+    return (inputFile, timeFile, statsFile, outputFile)
 
 
 def getData(fileName, header=0):
@@ -51,27 +49,43 @@ def getData(fileName, header=0):
     return df
 
 
+def buildColorsList(data, cmap):
+    normalize = colors.Normalize(vmax=data.max(), vmin=data.min())
+    colorList = cmap(normalize(data))
+    return colorList
+
+
 if __name__ == '__main__':
 
-    inputFile, timeFile, statsFile, paramsFile, outputFile \
+    inputFile, timeFile, statsFile, outputFile \
         = get_args(sys.argv[1:])
 
     data = getData(inputFile, header=None)
     time = getData(timeFile)
     statsData = getData(statsFile)
-    paramsData = getData(paramsFile)
 
     time = time.iloc[0:, 0].values
     sumAll = np.sum(data.iloc[:, :], axis=1)
     dataNorm = data.iloc[:, :].div(sumAll, axis=0).values
 
+    #cmap = plt.get_cmap('Blues', data.shape[1])
+    #cmap = plt.cm.get_cmap('magma_r', data.shape[1])
+    cmap = plt.cm.get_cmap('RdYlBu', data.shape[1])
+
+    #colors = cmap.colors
+    colorsIds = np.arange(0, data.shape[1], 1)
+    colors = buildColorsList(colorsIds, cmap)
     fig = plt.figure(figsize=(25, 20))
-    plt.stackplot(time, dataNorm.T)
+    sp = plt.stackplot(time, dataNorm.T, colors=colors)
 
     ax1 = fig.add_subplot(111)
     ax2 = ax1.twiny()
     ax1Ticks = ax1.get_xticks()
     ax2Ticks = ax1Ticks
+
+    def tick_function(X):
+        V = X
+        return ["%.3f" % z for z in V]
 
     ax2.set_xticks(ax2Ticks)
     ax2.set_xbound(ax1.get_xbound())
@@ -85,9 +99,16 @@ if __name__ == '__main__':
     ax1.set_xlabel('time [sytem]', fontsize='xx-large')
     ax2.set_xlabel('number of cells in system', fontsize='xx-large')
 
+    sm = plt.cm.ScalarMappable(cmap=cmap, 
+                               norm=plt.Normalize(vmin=dataNorm.min(),
+                                                  vmax=dataNorm.max()))
+    sm._A = []
+    colorbar = plt.colorbar(sm)
+    colorbar.set_label("parametr value", size='xx-large')
+
     outputFienName = outputFile+'.png'
-    plt.savefig(outputFienName, dpi=150)
-    # plt.show()
+    # plt.savefig(outputFienName, dpi=150)
+    plt.show()
     plt.cla()
     plt.clf()
     plt.close('all')
