731fb438733bb3dbedb25dd186c5f68cbad51e10
Marek Bawiec <Marek.Bawiec@pwr.edu.pl>
2019-07-16 17:24:12 +0200

    histogram parquet version

--- python/histogram.py
+++ python/histogram.py
@@ -4,57 +4,25 @@
 """ Muller like plots with params values as color decoded...
 """
 import numpy as np
-import csv
-import sys
-import os
-import getopt
 import matplotlib.pyplot as plt
+import fire
+import pyarrow.parquet as pq
 
 
-def get_args(argv):
-    inputFile = ''
-    statsFile = ''
-    thresholdFile = ''
-    outputFile = ''
+def histogram_plot(input_file, threshold, output_file):
+    data = pq.read_table(input_file,
+                         columns=['mutationId', 'mutationCount']).to_pandas()
+    sortedData = data.sort_values(by=['mutationCount'], ascending=False)
+    sortedData = sortedData.iloc[:threshold, :threshold]
+    data = np.array(sortedData)
 
-    try:
-        opts, args = getopt.getopt(argv, "hi:t:o:", [
-                        "iFile=", "threshold", "oFile="])
-    except getopt.GetoptError:
-        print('test.py -i <inputFile> -t <threshold> -o <outputFile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputFile> outputFile \
-                  -t <threshold> -o <outputFile>')
-            sys.exit()
-        elif opt in ("-i", "--iFile"):
-            inputFile = arg
-        elif opt in ("-t", "--threshold"):
-            threshold = arg
-        elif opt in ("-o", "--oFile"):
-            outputFile = arg
-
-    return (inputFile, threshold, outputFile)
-
-if __name__ == '__main__':
-
-    inputFile, threshold, outputFile = get_args(sys.argv[1:])
-
-    sortedData = os.popen('tail -n +2 '+inputFile+' | sort \
-                          -t ";" -k2,2rn --stable |  \
-                          head -n '+threshold).read()
-    print(sortedData)
-    data = csv.reader(sortedData.splitlines(), delimiter=';')
-    data = list(data)
-    data = np.array(data, dtype=int)
+    # data = np.delete(data, 0, axis=0)
+    ''' delete mutation_id=1, evry cell has this mutation '''
 
     localSum = np.sum(data.T[1][1, ])
     totalSum = data.T[1][0]
     perCent = localSum/totalSum*100
 
-    # data = np.delete(data, 0, axis=0)
-    ''' delete mutation_id=1, evry cell has this mutation '''
     labels = np.array(data).T
     index = np.arange(data.shape[0])
 
@@ -66,17 +34,12 @@ if __name__ == '__main__':
     plt.xticks(index, labels[0])
     plt.grid()
 
-    textstr = '\n'.join((
-                        'sum=%d' % (localSum),
-                        'total nb of cells=%d' % (totalSum),
-                        'percent of all cells in system=%.2f%%' % perCent))
-
-    ax.text(x=0.75, y=0.85, s=textstr, transform=ax.transAxes)
-
     for idx, val in enumerate(labels[1]):
         plt.text(x=idx, y=val, s=f"{val}", fontdict=dict(fontsize=12))
 
     # plt.show()
-    plt.savefig(outputFile)
+    plt.savefig(output_file, bbox_inches='tight', dpi=150)
 
-print("That's all.")
+if __name__ == '__main__':
+    fire.Fire(histogram_plot)
+    print("That's all.")
